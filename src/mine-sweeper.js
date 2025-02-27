46af5b6726989c04c96bbf16a6d2226ef9496176
Ryhus <ryhusyevhen@gmail.com>
2025-02-27 17:06:43 +0100

    feat: add solution for mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const flatArr = matrix.flat();
+  const rowLength = matrix[0].length;
+
+  const countBombsArr = [];
+  let countBombs = 0;
+  for (let i = 0; i < flatArr.length; i += 1) {
+    const reminder = i % rowLength;
+    if (reminder !== 0) {
+      if (flatArr[i - 1] === true) countBombs += 1;
+      if (flatArr[i - (rowLength + 1)] === true) countBombs += 1;
+      if (flatArr[i + (rowLength - 1)] === true) countBombs += 1;
+    }
+    if (reminder !== rowLength - 1) {
+      if (flatArr[i + 1] === true) countBombs += 1;
+      if (flatArr[i - (rowLength - 1)] === true) countBombs += 1;
+      if (flatArr[i + (rowLength + 1)] === true) countBombs += 1;
+    }
+
+    if (flatArr[i - rowLength] === true) countBombs += 1;
+    if (flatArr[i + rowLength] === true) countBombs += 1;
+
+    countBombsArr.push(countBombs);
+    countBombs = 0;
+  }
+
+  let k = 0;
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      matrix[i][j] = countBombsArr[k];
+      k += 1;
+    }
+  }
+  return matrix;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
