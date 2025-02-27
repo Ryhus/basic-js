9b0c851d7ff49be0cba5153a0ef62088303dd4a1
Ryhus <ryhusyevhen@gmail.com>
2025-02-27 09:42:33 +0100

    feat: add solution for encode-line task

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let count = 1;
+  let encodedLine = [];
+  let previousLetter;
+  const numbers = [];
+  for (let letter of str) {
+    previousLetter === letter ? (count += 1) : (count = 1);
+    previousLetter = letter;
+    numbers.push(count);
+  }
+  numbers.push(0);
+  for (let i = 1; i < numbers.length; i += 1) {
+    console.log(numbers[i], numbers[i - 1]);
+    if (numbers[i] < numbers[i - 1]) {
+      encodedLine.push(String(numbers[i - 1]), str[i - 1]);
+      console.log(encodedLine);
+    } else if (numbers[i - 1] === numbers[i]) encodedLine.push(str[i - 1]);
+  }
+  console.log(numbers);
+  return encodedLine.filter((element) => element != "1").join("");
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
