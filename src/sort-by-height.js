ffcc330c1ba10f21007922ef35c029527e6442eb
Ryhus <ryhusyevhen@gmail.com>
2025-03-01 13:45:46 +0100

    feat: add solution for sort-by-height task

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  if (!arr.some((element) => element === -1)) return arr.sort((a, b) => a - b);
+  const minus1indexes = [];
+  arr.forEach((element, index) => {
+    if (element === -1) minus1indexes.push(index);
+  });
+
+  arr = arr.filter((element) => element !== -1).sort((a, b) => a - b);
+
+  minus1indexes.forEach((element) => {
+    if (element === 0) {
+      arr = [-1].concat(arr);
+    } else {
+      arr = arr.slice(0, element).concat([-1]).concat(arr.slice(element));
+    }
+  });
+  return arr;
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
